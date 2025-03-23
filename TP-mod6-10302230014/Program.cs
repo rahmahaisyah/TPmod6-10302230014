622c6167350313fcc4aa56cb1306784f5998ed7a
rahmahaisyah <rahmahasyh@example.com>
2025-03-24 00:53:31 +0700

    menambahkan class SayaTubeVideo

--- TP-mod6-10302230014/Program.cs
+++ TP-mod6-10302230014/Program.cs
@@ -1,10 +1,40 @@
 ﻿namespace TP_mod6_10302230014
 {
+
+    class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            Random rand = new Random();
+            this.id = rand.Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            this.playCount += count;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("Video ID: " + this.id);
+            Console.WriteLine("Title: " + this.title);
+            Console.WriteLine("Play Count: " + this.playCount);
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - [Rahmah Aisyah]");
+            video.IncreasePlayCount(10);
+            video.PrintVideoDetails();
         }
     }
 }
