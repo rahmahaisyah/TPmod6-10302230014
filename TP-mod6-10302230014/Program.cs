fadb1344117dd8652d9b2aa86b49c2af720c257e
rahmahaisyah <rahmahasyh@example.com>
2025-03-23 21:56:27 +0700

    Add project files.

---
+++ TP-mod6-10302230014/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TP_mod6_10302230014
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
